49093540e79e21ab2893c2137f657924a42eb680
Beastie PC <gskondratenko@opora.org.ua>
2019-09-12 10:50:11 +0300

    Fix scrape name at faction_name_to_id

--- scraper.rb
+++ scraper.rb
@@ -78,7 +78,7 @@ def person_name_to_id(abbreviated_name, faction_name)
 end
 
 def faction_name_to_id(name)
-  @factions ||= morph_scraper_query("openaustralia/ukraine_verkhovna_rada_deputies",
+  @factions ||= morph_scraper_query("beastie87/ukraine_verkhovna_rada_deputies",
                                     "SELECT DISTINCT COALESCE(faction_id, 'party/Позафракційні') AS faction_id, COALESCE(faction, 'Позафракційні') AS faction FROM 'data'")
   if faction = @factions.find { |f| f["faction"].include? name }
     faction["faction_id"]
